c0e02c4bb56577515c1858b9baa26412bda18aa7
Ayaz Somani (Aluance Digital) <ayazs@aluance.io>
2022-01-23 12:48:15 -0500

    Implemented dictionary functionality and tests.

--- master/internal/dictionary/dictionary.go
+++ master/internal/dictionary/dictionary.go
@@ -2,36 +2,51 @@ package dictionary
 
 import (
 	"bufio"
-	"fmt"
+	"math/rand"
 	"os"
 )
 
+const CONFIG_DICTIONARY_FILENAME = "google-10000-english-usa-no-swears-medium.txt"
+
 func GenerateWord() (string, error) {
-	return "", nil
+	word := "blank"
+	if max := wordleDict.size(); max > 0 {
+		index := rand.Intn(max)
+		word = wordleDict.words[index]
+	}
+
+	return word, nil
 }
 
 func IsWordValid(w string) bool {
+	if member, ok := wordleDict.wordMap[w]; ok {
+		return member
+	}
+
 	return false
 }
 
-func Initialize(file string) error {
-	if len(file) < 1 {
-		return fmt.Errorf("invalid filename")
+func Initialize(filename string) error {
+	if len(filename) < 1 {
+		filename = CONFIG_DICTIONARY_FILENAME
 	}
 
-	f, err := os.Open(file)
+	f, err := os.Open(filename)
 	if err != nil {
 		return err
 	}
 	defer f.Close()
 
-	// read the file line by line using scanner
-	scanner := bufio.NewScanner(f)
+	wordleDict.words = []string{}
+	wordleDict.wordMap = make(map[string]bool)
 
+	// Load only 5-letter words from the file
+	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
 		word := scanner.Text()
 		if len(word) == 5 {
 			wordleDict.words = append(wordleDict.words, word)
+			wordleDict.wordMap[word] = true
 		}
 	}
 
@@ -43,7 +58,12 @@ func Initialize(file string) error {
 }
 
 type dict struct {
-	words []string
+	words   []string
+	wordMap map[string]bool
+}
+
+func (d dict) size() int {
+	return len(d.words)
 }
 
 var wordleDict = &dict{}
--- master/internal/dictionary/dictionary_test.go
+++ master/internal/dictionary/dictionary_test.go
@@ -1,28 +1,68 @@
 package dictionary
 
 import (
+	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestGenerateWord(t *testing.T) {
+const TEST_DICTIONARY_LENGTH = 1367
+const TEST_DICTIONARY_FILENAME = "google-10000-english-usa-no-swears-medium.txt"
 
+func TestGenerateWord(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+
+	err := Initialize(TEST_DICTIONARY_FILENAME)
+	require.NoError(err)
+
+	word, err := GenerateWord()
+	assert.NoError(err)
+	assert.Equal(5, len(word))
 }
 
 func TestIsWordValid(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
 
+	err := Initialize(TEST_DICTIONARY_FILENAME)
+	require.NoError(err)
+
+	// Test valid words
+	testWords := []string{"anime", "bills", "drawn", "lives", "nodes"}
+	for _, tw := range testWords {
+		assert.True(IsWordValid(tw))
+	}
+
+	// Test invalid words
+	testWords = []string{"xxxxx", "whizz", "bangs"}
+	for _, tw := range testWords {
+		assert.False(IsWordValid(tw))
+	}
 }
 
 func TestInitialize(t *testing.T) {
 	assert := assert.New(t)
+	rand.Seed(time.Now().UnixNano())
 
-	filename := "google-10000-english-usa-no-swears-medium.txt"
-
-	err := Initialize(filename)
+	// Test standard initialization
+	err := Initialize("")
 	assert.NoError(err)
 
+	// Use controled initialization
+	err = Initialize(TEST_DICTIONARY_FILENAME)
+	assert.NoError(err)
+
+	// Make sure that the dictionary is not empty
 	assert.NotZero(len(wordleDict.words))
-	assert.Equal(1367, len(wordleDict.words))
-	// assert.Equal(wordleDict.words[1300], "bless")
+	assert.Equal(TEST_DICTIONARY_LENGTH, len(wordleDict.words))
+	assert.Equal(TEST_DICTIONARY_LENGTH, len(wordleDict.wordMap))
+
+	// Test that the length of a random word is 5
+	assert.Equal(5, len(wordleDict.words[rand.Intn(TEST_DICTIONARY_LENGTH)]))
+
+	// assert.Equal(wordleDict.words[rand.Intn(TEST_DICTIONARY_LENGTH)], "bless")
 }
