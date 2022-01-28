8a70bbb843b17960319ca4d0b6ceb9bccce4c9a6
Ayaz Somani (Aluance Digital) <ayazs@aluance.io>
2022-01-28 08:02:55 -0500

    Moved player code to a separate repo.

--- player/internal/config/errors.go
+++
@@ -1,7 +0,0 @@
-package config
-
-import "errors"
-
-var (
-	ErrFilepath = errors.New("invalid filepath")
-)
--- player/internal/playerbot/errors.go
+++
@@ -1,9 +0,0 @@
-package playerbot
-
-import "errors"
-
-var (
-	ErrNilDictionary  = errors.New("dictionary is nil")
-	ErrInvalidBotName = errors.New("invalid bot name")
-	ErrFailedAttempt  = errors.New("failed attempt")
-)
