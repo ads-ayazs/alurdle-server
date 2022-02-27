b222686e2599ccd9c98d00c89559f859b01c312f
Ayaz Somani (Aluance Digital) <ayazs@aluance.io>
2022-02-27 15:37:45 -0500

    Godoc comments for config package

--- master/internal/config/config.go
+++ master/internal/config/config.go
@@ -1,3 +1,10 @@
+/*
+Package config centralizes shared configuration across the entire application.
+
+Key functions:
+	LoadEmbedFile(fp string) - Loads a file by name from data embedded in the executable.
+	RootDir() - Returns the root folder from where the binary executable resides.
+*/
 package config
 
 import (
@@ -8,15 +15,28 @@ import (
 	"runtime"
 )
 
+// Port where the API is served.
 const CONFIG_API_PORT = 8080
 
 // const CONFIG_DICTIONARY_FILENAME = "google-10000-english-usa-no-swears-medium.txt"
+
+// Filename of the default dictionary.
 const CONFIG_DICTIONARY_FILENAME = "corncob_lowercase.txt"
+
+// Filepath of the default dictionary.
 const CONFIG_DICTIONARY_FILEPATH = "data/" + CONFIG_DICTIONARY_FILENAME
+
+// Game word length.
 const CONFIG_GAME_WORDLENGTH = 5
+
+// Game maximum permitted attempts.
 const CONFIG_GAME_MAXATTEMPTS = 12
+
+// Game maximum valid attempts.
 const CONFIG_GAME_MAXVALIDATTEMPTS = 6
 
+
+// RootDir returns the runtime root folder.
 func RootDir() string {
 	_, b, _, _ := runtime.Caller(0)
 	d := path.Join(path.Dir(b))
@@ -26,11 +46,14 @@ func RootDir() string {
 //go:embed data/*
 var embFS embed.FS
 
+// LoadEmbedFile loads the given file path from the data embedded in the binary.
 func LoadEmbedFile(fp string) (fs.File, error) {
+	// Validate fp
 	if len(fp) < 1 {
 		return nil, ErrFilepath
 	}
 
+	// Open the file from the embedded file system.
 	f, err := embFS.Open(fp)
 	if err != nil {
 		return nil, err
--- master/internal/config/errors.go
+++ master/internal/config/errors.go
@@ -2,6 +2,7 @@ package config
 
 import "errors"
 
+// Errors returned by functions in this package.
 var (
 	ErrFilepath = errors.New("invalid filepath")
 )
