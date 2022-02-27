576d80c7d9f97134d5c28b2b1d7f500fd49916dd
Ayaz Somani (Aluance Digital) <ayazs@aluance.io>
2022-02-27 14:45:28 -0500

    Godoc comments for the dictionary package.

--- master/internal/dictionary/dictionary.go
+++ master/internal/dictionary/dictionary.go
@@ -1,3 +1,12 @@
+/*
+Package dictionary implements a thread-safe dictionary used to generate and
+validate game words.
+
+Key functions:
+	GenerateWord() - Returns a random word from the dictionary.
+	Initialize(filename string) - Loads a dictionary from the given file name.
+	IsWordValid(w string) - Returns true if the given word exists in the dictionary.
+*/
 package dictionary
 
 import (
@@ -11,14 +20,20 @@ import (
 	"github.com/matryer/resync"
 )
 
+// GenerateWord returns a random word from the dictionary.
+//
+// Returns an error if the dictionary fails to initialize.
 func GenerateWord() (string, error) {
+	// Initialize the dictionary
 	if err := Initialize(""); err != nil {
 		return "", err
 	}
 
+	// Obtain a read lock
 	wordleDict.mu.RLock()
 	defer wordleDict.mu.RUnlock()
 
+	// Return a random word or "blank" by default.
 	word := "blank"
 	if max := wordleDict.size(); max > 0 {
 		index := rand.Intn(max)
@@ -28,14 +43,18 @@ func GenerateWord() (string, error) {
 	return word, nil
 }
 
+// IsWordValid returns true if the given word exists in the dictionary.
 func IsWordValid(w string) bool {
+	// Initialize the dictionary
 	if err := Initialize(""); err != nil {
 		return false
 	}
 
+	// Ontain a read lock
 	wordleDict.mu.RLock()
 	defer wordleDict.mu.RUnlock()
-	
+
+	// Check if the word is in the dictionary
 	if member, ok := wordleDict.wordMap[strings.ToLower(w)]; ok {
 		return member
 	}
@@ -43,16 +62,22 @@ func IsWordValid(w string) bool {
 	return false
 }
 
+// Initialize loads a dictionary from the given file name.
+//
+// When filename is empty, it attempts to load the default dictionary. The
+// function ensures that a dictionary is only loaded once. Returns an error if
+// it is unable to load from the file.
 func Initialize(filename string) error {
-
+	// Ontain a write lock
 	wordleDict.mu.Lock()
 	defer wordleDict.mu.Unlock()
 
-	// Only initialized dictionary once
+	// Only initialize dictionary once
 	if wordleDict.initalized {
 		return nil
 	}
 
+	// Open the dictonary file
 	if len(filename) < 1 {
 		filename = config.CONFIG_DICTIONARY_FILEPATH
 	}
@@ -87,6 +112,7 @@ func Initialize(filename string) error {
 	return nil
 }
 
+// dict struct stores the entire dictionary in memory.
 type dict struct {
 	init_once  resync.Once
 	initalized bool
@@ -95,10 +121,12 @@ type dict struct {
 	wordMap    map[string]bool
 }
 
+// size returns the number of words in the dictionary
 func (d *dict) size() int {
 	return len(d.words)
 }
 
+// reset clears and resets the loaded dictionary for testing.
 func (d *dict) reset() {
 	d.mu.Lock()
 	defer d.mu.Unlock()
@@ -109,4 +137,5 @@ func (d *dict) reset() {
 	d.initalized = false
 }
 
+// wordleDict is the singleton dict variable.
 var wordleDict = &dict{initalized: false, words: []string{}, wordMap: make(map[string]bool)}
